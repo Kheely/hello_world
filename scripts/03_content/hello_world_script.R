d91ee04fbdbc562d85f22cd01174358255344957
christine-martin <cmartin1624@gmail.com>
2025-09-09 10:05:39 -0400

    branching

--- scripts/03_content/hello_world_script.R
+++ scripts/03_content/hello_world_script.R
@@ -6,8 +6,13 @@ library(tidyverse)
 data("data_lionfish")
 
 # Create a simple plot
+ggplot(data_lionfish,
+       aes(x = total_length_mm, y = total_weight_gr, color = "green")) +
+  geom_point() +
+  facet_wrap(~site)
+
 p <- ggplot(data_lionfish,
-       aes(x = total_length_mm, y = total_weight_gr, color = "pink")) +
+       aes(x = total_length_mm, y = total_weight_gr, color = "green")) +
   geom_point() +
   facet_wrap(~site)
 
